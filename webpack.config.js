1599b5f811c76312f0e2b5d07d420dfc8eba9ccd
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-01-26 16:35:30 +0200

    commit_second

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
-module.exports = { 
+module.exports = {
+  output: {
+   path: path.resolve(__dirname, "review_build"),
+   filename: "[name].[hash].js"
+  },
   module: {
     rules: [
     {
